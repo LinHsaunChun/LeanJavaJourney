c1cdd73f2c7545bdfdaea03ef8ff3d5d3784129c
Lin HsaunChun <lin.hsuan.chun@gmail.com>
2015-02-17 13:56:22 +0800

    字元印出練習
    練習項目：
    1.宣告一個公開的類別
    2.宣告一個公開的程式
    3.印出字元

---
+++ helloworld/helloworld.java
@@ -0,0 +1,17 @@
+
+package leaned;
+/*
+ * 程式目的：學習宣告類別與印出字元
+ * 程式說明：
+ * 			1.宣告一個公開的類別
+ * 			2.宣告一個公開的程式
+ * 			3.印出字元
+ */
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!World!");
+	}
+
+}
