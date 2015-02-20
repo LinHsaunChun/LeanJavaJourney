ad01d9c925791e73c42588097e79b6facbba1aea
Lin HsaunChun <lin.hsuan.chun@gmail.com>
2015-02-20 23:33:39 +0800

    用*印出菱形

---
+++ TestForApplication/PrintDiamond.java
@@ -0,0 +1,14 @@
+package TestForApplication;
+
+public class PrintDiamond {
+
+	public static void main(String[] args) 
+	{
+		//檔名:以字元*印出菱形
+		System.out.println("   *  ");
+		System.out.println("  ***  ");
+		System.out.println(" *****  ");
+		System.out.println("  ***  ");
+		System.out.println("   *  ");
+	}
+}
